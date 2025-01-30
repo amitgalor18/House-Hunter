768ce311be24e114ad1988386b3b209a7d74f093
amitgalor18 <46008959+amitgalor18@users.noreply.github.com>
2025-01-31 00:50:35 +0200

    fix vite issue with gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/House-Hunter/',
+  base: "/House-Hunter/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
